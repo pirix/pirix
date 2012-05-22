d001aa76b9811c96399c50f63579583801a9e1e5
Thomas Gatzweiler <mail@thomasgatzweiler.com>
2012-05-23 01:43:37 +0200

    implement heap_init

--- include/ix/kheap.h
+++ include/ix/kheap.h
@@ -1,9 +1,22 @@
-#ifndef IX_KMALLOC_H
-#define IX_KMALLOC_H
+#pragma once
 
 typedef unsigned size_t;
 
+typedef struct heap_state {
+    int valid;
+    unsigned* vmem_mark;
+    size_t used_bytes;
+    size_t block_count;
+} heap_state;
+
+typedef struct heap {
+    unsigned* start;
+    unsigned* end;
+    heap_state state;
+} heap;
+
+void kheap_init();
 void* kmalloc(size_t size);
 void kfree(void* addr);
 
-#endif
+extern heap kheap;
--- kernel/kheap.c
+++ kernel/kheap.c
@@ -1,9 +1,27 @@
 #include <ix/kheap.h>
 #include <ix/memory.h>
 #include <ix/paging.h>
+#include <string.h>
+
+heap kheap;
+
+void kheap_init() {
+    memset(&kheap, 0, sizeof(heap));
+    kheap.start = 0;
+    kheap.end = 0;
+
+    kheap.state.used_bytes = 0;
+    kheap.state.block_count = 0;
+    kheap.state.vmem_mark = kheap.start + 4096;
+
+    unsigned phys = memory_alloc();
+    unsigned virt = paging_map_kernel(phys);
+
+    kheap.state.valid = 1;
+}
 
 void* kmalloc(size_t size) {
-    return paging_map_kernel((unsigned)memory_alloc());
+    return 0;
 }
 
 void kfree(void* addr) {
