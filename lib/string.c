10291c325216e86a12a90fe538f6d72ba906cf6e
Thomas Gatzweiler <mail@thomasgatzweiler.com>
2012-05-29 18:40:31 +0200

    implement strdup, strchr and strtok_r

--- lib/string.c
+++ lib/string.c
@@ -1,3 +1,4 @@
+#include <pirix/kheap.h>
 #include <string.h>
 
 void* memcpy(void* dest, const void* src, size_t num) {
@@ -34,6 +35,13 @@ char* strncpy(char* dest, const char* src, size_t num) {
     return rdest;
 }
 
+char* strdup(const char* src) {
+    size_t len = strlen(src)+1;
+    char* new = kmalloc(sizeof(char)*len);
+    memcpy(new, src, len);
+    return new;
+}
+
 char* strcat(char* dest, const char* src) {
     char* rdest = dest;
     while (*dest) dest++;
@@ -54,6 +62,42 @@ int strcmp(const char* str1, const char* str2) {
         if (*(str1++) != *(str2++)) return i;
     }
     return 0;
+}
+
+char* strchr(const char* str, int c) {
+    while (1) {
+        if (*str == c) return (char*)str;
+        else if (!(*str)) return NULL;
+        else str++;
+    }
+}
+
+char* strtok_r(char* str, const char* delimiters, char** save) {
+    if (!str) str = *save;
+
+    // skip delemiters at the beginning of str
+    while (strchr(delimiters, *str)) {
+        if (!(*str)) {
+            *save = str;
+            return NULL;
+        }
+        str++;
+    }
+
+    char* out = str;
+
+    // find next delimiter
+    while (!strchr(delimiters, *str)) str++;
+
+    if (*str) {
+        *str = 0;
+        *save = str + 1;
+    }
+    else {
+        *save = str;
+    }
+
+    return out;
 }
 
 size_t strlen(const char* str) {
