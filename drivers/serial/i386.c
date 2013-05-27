ff7442d1a353542063d55fc0faa33b6a66c1addf
Thomas Gatzweiler <mail@thomasgatzweiler.com>
2013-05-27 18:41:52 +0200

    i386 serial: add temporary video output

--- drivers/serial/i386.c
+++ drivers/serial/i386.c
@@ -1,4 +1,51 @@
 #include <arch/i386.h>
+#include <string.h>
+
+static short* vmem = (short*)0xc00b8000;
+static int vx = 0, vy = 0;
+
+void video_scroll() {
+    memcpy(vmem, vmem+25, sizeof(short)*80*24);
+    memset(vmem+(80*24), 0, sizeof(short)*25);
+    vy--;
+}
+
+void video_cursor() {
+    unsigned pos = vy * 80 + vx;
+    outb(0x3D4, 14);
+    outb(0x3D5, pos >> 8);
+    outb(0x3D4, 15);
+    outb(0x3D5, pos);
+}
+
+void video_putc(char c) {
+    switch(c) {
+    case '\n':
+        vx = 0;
+        vy++;
+        break;
+
+    case '\b':
+        vx--;
+        video_putc(' ');
+        vx--;
+        break;
+
+    default:
+        vmem[vx+vy*80] = 0x0f << 8 | c;
+        vx++;
+    }
+
+    if (vx >= 80) { vx = 0; vy++; }
+    if (vy >= 25) video_scroll();
+    video_cursor();
+}
+
+void video_clear() {
+    memset(vmem, 0, sizeof(short)*80*25);
+    vx = vy = 0;
+    video_cursor();
+}
 
 #define PORT 0x3f8
 
@@ -10,11 +57,13 @@ void serial_init() {
     outb(PORT+3, 0x03);
     outb(PORT+2, 0xC7);
     outb(PORT+4, 0x0B);
+    video_clear();
 }
 
 void serial_putc(char c) {
     while ((inb(PORT+5) & 0x20) == 0);
     outb(PORT, c);
+    video_putc(c);
 }
 
 char serial_getc() {
