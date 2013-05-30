e455f2a2d107bffc9748af36326919c704b9c722
Thomas Gatzweiler <mail@thomasgatzweiler.com>
2013-05-31 01:08:34 +0200

    libpirix: move kill into signal.c

--- lib/pirix/kill.c
+++
@@ -1,5 +0,0 @@
-#include <sys/pirix.h>
-
-int kill(int pid, int sig) {
-    return sys_kill(pid, sig);
-}
--- lib/pirix/signal.c
+++ lib/pirix/signal.c
@@ -1,4 +1,6 @@
 #include <signal.h>
+#include <sys/pirix.h>
+
 
 int sigprocmask(int how, const sigset_t* set, sigset_t* oldset) {
     return -1;
@@ -6,4 +8,8 @@ int sigprocmask(int how, const sigset_t* set, sigset_t* oldset) {
 
 int sigaction(int signum, const struct sigaction* act, struct sigaction* oldact) {
     return 0;
+}
+
+int kill(int pid, int sig) {
+    return sys_kill(pid, sig);
 }
