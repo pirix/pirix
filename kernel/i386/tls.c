a452772e0108920255adf2c8e0950bac996afd57
Thomas Gatzweiler <mail@thomasgatzweiler.com>
2013-06-04 02:37:02 +0200

    i386/tls.c: fix gdt entry creation

--- kernel/i386/tls.c
+++ kernel/i386/tls.c
@@ -6,7 +6,6 @@
 
 void tls_activate(unsigned desc[2]) {
     extern unsigned gdt_tls[2];
-    if (!desc[1]) return;
     gdt_tls[0] = desc[0];
     gdt_tls[1] = desc[1];
 }
@@ -14,14 +13,12 @@ void tls_activate(unsigned desc[2]) {
 int tls_set(struct tls_desc* desc) {
     thread* t = scheduler_current_thread();
 
-    t->tls[0] = desc->limit & 0xffff;
-    t->tls[0] |= desc->base << 16;
+    t->tls[0] = desc->limit & 0xffff | desc->base << 16;
 
-    t->tls[1] = (desc->base >> 16) & 0xff;
-    t->tls[1] |= 0b11110110 << 8;
-    t->tls[1] |= desc->limit & 0xf0000;
-    t->tls[1] |= 0b11 << 20;
-    t->tls[1] |= desc->base & 0xff000000;
+    t->tls[1] = (desc->base >> 16) & 0xff
+                | 0b11110010 << 8 | 0b11 << 22
+                | desc->limit & 0xf0000
+                | desc->base & 0xff000000;
 
     tls_activate(t->tls);
 
