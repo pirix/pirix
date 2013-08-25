1cb0e7a1a965d7d294523f5bedbec4fae9d48d32
Thomas Gatzweiler <mail@thomasgatzweiler.com>
2013-08-25 23:37:18 +0200

    [arm] change register type to uint32_t

--- include/arch/arm/cpu.h
+++ include/arch/arm/cpu.h
@@ -1,11 +1,13 @@
 #pragma once
 
+#include <arch/types.h>
+
 typedef struct registers {
-    unsigned usr_r13, usr_r14;
-    unsigned cpsr, svc_r14;
-    unsigned r0, r1, r2, r3, r4, r5, r6;
-    unsigned r7, r8, r9, r10, r11, r12;
-    unsigned r15, spsr;
+    uint32_t usr_r13, usr_r14;
+    uint32_t cpsr, svc_r14;
+    uint32_t r0, r1, r2, r3, r4, r5, r6;
+    uint32_t r7, r8, r9, r10, r11, r12;
+    uint32_t r15, spsr;
 } __attribute__((packed)) registers;
 
 static inline void registers_init(registers* regs, void* entry) {
@@ -19,8 +21,8 @@ static inline void registers_set_kernel_mode(registers* regs) {
     regs->spsr = 0x5F;
 }
 
-static inline void registers_set_stack(registers* regs, unsigned* stack) {
-    regs->usr_r13 = (unsigned)stack;
+static inline void registers_set_stack(registers* regs, uintptr_t stack) {
+    regs->usr_r13 = (uint32_t)stack;
 }
 
 static inline void irq_enable() {
