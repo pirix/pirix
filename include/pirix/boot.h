b910e2a9d3c787077cd7a22f5598c881e8fae90c
Thomas Gatzweiler <mail@thomasgatzweiler.com>
2013-08-27 02:24:09 +0200

    refactor multiboot

--- include/pirix/boot.h
+++ include/pirix/boot.h
@@ -1,17 +1,19 @@
 #pragma once
 
+#include <pirix/types.h>
+
 #define BOOT_ARGS_MAX 100
 #define BOOT_MOD_MAX 15
 #define BOOT_MOD_ARGS_MAX 30
 
 typedef struct boot_module {
-    unsigned* addr;
-    unsigned size;
+    uintptr_t addr;
+    size_t size;
     char args[BOOT_MOD_ARGS_MAX];
 } boot_module;
 
 typedef struct boot_info {
     char args[BOOT_ARGS_MAX];
     boot_module modules[BOOT_MOD_MAX];
-    unsigned mod_count;
+    size_t mod_count;
 } boot_info;
--- kernel/arch/i386/multiboot.c
+++ kernel/arch/i386/multiboot.c
@@ -3,33 +3,41 @@
 #include <pirix/string.h>
 #include <pirix/boot.h>
 
-boot_info* multiboot_init(unsigned long mb_magic, multiboot_info* mb_info) {
-    static boot_info binfo;
+static boot_info binfo;
 
+static void multiboot_modules(int count, multiboot_mod_list* mod) {
+    if (count < 1) {
+        panic("no multiboot modules found");
+    }
+
+    if (count > BOOT_MOD_MAX) {
+        panic("too many multiboot modules");
+    }
+
+    for (int i = 0; i < count; i++) {
+        binfo.modules[i].addr = (uintptr_t)mod->start;
+        binfo.modules[i].size = mod->end - mod->start;
+        strncpy(binfo.modules[i].args, (char*)mod->args, BOOT_MOD_ARGS_MAX);
+        mod++;
+    }
+}
+
+static void multiboot_mmap(int length, multiboot_mmap_entry* mmap) {
+    for (int i = 0; i < length; i++) {
+        //! TODO
+        mmap++;
+    }
+}
+
+boot_info* multiboot_init(unsigned long mb_magic, multiboot_info* mb_info) {
     if (mb_magic != MULTIBOOT_BOOTLOADER_MAGIC) {
         panic("invalid multiboot magic");
     }
 
     strncpy(binfo.args, (char*)mb_info->cmdline, BOOT_ARGS_MAX);
 
-    if (mb_info->mod_count < 1) {
-        panic("no multiboot modules found");
-    }
-
-    if (mb_info->mod_count > BOOT_MOD_MAX) {
-        panic("too many multiboot modules");
-    }
-
-    binfo.mod_count = mb_info->mod_count;
-
-    multiboot_mod_list* mod = (multiboot_mod_list*)mb_info->mod_addr;
-
-    for (int i = 0; i < binfo.mod_count; i++) {
-        binfo.modules[i].addr = (unsigned*)mod->start;
-        binfo.modules[i].size = mod->end - mod->start;
-        strncpy(binfo.modules[i].args, (char*)mod->args, BOOT_MOD_ARGS_MAX);
-        mod++;
-    }
+    multiboot_modules(mb_info->mod_count, (void*)mb_info->mod_addr);
+    multiboot_mmap(mb_info->mmap_length, (void*)mb_info->mmap_addr);
 
     return &binfo;
 }
